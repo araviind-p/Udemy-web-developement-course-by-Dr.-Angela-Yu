dc95763a0eb4367dcd5be7d739362ec48ee66322
Aravind <aravindparavind10@gmail.com>
2024-02-02 12:36:25 +0530

    Drum Kit

---
+++ Drum Kit/index.js
@@ -0,0 +1,43 @@
+document.addEventListener('keypress', (event) => {
+
+    var name = event.key;
+
+    var activeButton = document.querySelector("." + name);
+    activeButton.classList.add("pressed");
+    setTimeout(() => activeButton.classList.remove("pressed"), 200);
+
+    switch (name) {
+        case 'w':
+            var audio = new Audio('sounds/crash.mp3');
+            audio.play();
+            break;
+        case 'a':
+            var audio = new Audio('sounds/kick-bass.mp3');
+            audio.play();
+            break;
+        case 's':
+            var audio = new Audio('sounds/snare.mp3');
+            audio.play();
+            break;
+        case 'd':
+            var audio = new Audio('sounds/tom-1.mp3');
+            audio.play();
+            break;
+        case 'j':
+            var audio = new Audio('sounds/tom-2.mp3');
+            audio.play();
+            break;
+        case 'k':
+            var audio = new Audio('sounds/tom-3.mp3');
+            audio.play();
+            break;
+        case 'l':
+            var audio = new Audio('sounds/tom-4.mp3');
+            audio.play();
+            break;
+        default:
+    }
+})
+
+
+
