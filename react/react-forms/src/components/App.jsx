0ee554479211c77bc430286d352a4f30aa646a15
Aravind <aravindparavind10@gmail.com>
2024-06-08 22:12:38 +0530

    react challenges

---
+++ react/react-forms/src/components/App.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+function App() {
+
+  const [name, setName] = useState("");
+  const [names, setNames] = useState("");
+  function handleChange(event){
+    setName(event.target.value)
+  }
+  function handleClick(event){
+    setNames(name);
+    event.preventDefault();
+  }
+
+  return (
+    <div className="container">
+      <h1>Hello {names}</h1>
+      <form onSubmit={handleClick}>
+      <input 
+        onChange={handleChange}
+        type="text" 
+        placeholder="What's your name?" 
+        value={name}
+      />
+      
+      <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
